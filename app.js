bc09858c4d348e4b9577e8c7deafb72c932bca48
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-04-09 15:39:57 +0200

    Connect mongodb

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
+
+
+mongoose.connect('mongodb+srv://louannebuisson:q3wVyqscckk7htvl@mon-vieux-grimoire.sj4vddb.mongodb.net/?retryWrites=true&w=majority&appName=mon-vieux-grimoire',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 //CORS
 app.use((req, res, next) => {

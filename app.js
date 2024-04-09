a66dcf657d7bae0357c983e69488b971f2c6408e
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-04-09 16:36:08 +0200

    Put & Delete /api/books/:id + Get bestrating

--- app.js
+++ app.js
@@ -32,6 +32,22 @@ app.use((req, res, next) => {
     .catch(error => res.status(400).json({error}));
   });
 
+//   app.post('api/books/:id/rating', (res, req, next) => {
+
+//   })
+
+app.put('/api/books/:id', (req, res, next) => {
+    Book.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(() => res.status(200).json({message: 'Livre modifié'}))
+        .catch(error => res.status(400).json({error}));
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+    Book.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({message: 'Livre supprimé'}))
+        .catch(error => res.status(400).json({error}));
+});
+
   app.get('/api/books', (req, res, next) =>{
     Book.find()
         .then(books => res.status(200).json(books))
@@ -42,6 +58,14 @@ app.use((req, res, next) => {
     Book.findOne({_id: reqparams.id})
         .then(book => res.status(200).json(book))
         .catch(error => res.status(404).json({error}));
+  })
+
+  app.get('/api/books/bestrating', (req, res, next) => {
+    Book.find()
+        .sort({averageRating: -1})
+        .limit(3)
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(500).json({error}));
   })
 
 // app.get('/api/books', (req, res, next) => {

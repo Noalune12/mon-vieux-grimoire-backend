cc8baec52a7ca2321fc411c63c5432c70d0b5d18
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-04-10 10:06:21 +0200

    Login Created

--- controllers/User.js
+++ controllers/User.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
@@ -16,5 +17,33 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({message: 'Paire identifiant/mot de passe incorrect'})
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({message: 'Paire identifiant/mot de passe incorrect'})
+                    } 
+                    const token = jwt.sign(
+                        { userId: user._id },
+                        'RANDOM_TOKEN_SECRET',
+                        { expiresIn: '24h' }
+                    );
+                        res.status(200).json({
+                            userId: user._id,
+                            token: token
+                            // token: jwt.sign(
+                            //     {userId: user._id},
+                            //     'RANDOM_TOKEN_SECRET',
+                            //     {expiresIn:'24h'}
+                            // )
+                        });
+                        console.log(token);       
+                })
+                .catch(error => res.status(500).json({error}));   
+        })
+        .catch(error => res.status(500).json({error}));
 };
